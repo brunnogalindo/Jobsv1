af1e48dfc7db330190b9f67c53b304cf2a682a05
Bruno Galindo <brunodsgalindo@hotmail.com>
2020-06-08 19:46:35 -0300

    Projeto Criado

---
+++ src/main/java/com/jobs/jobsv1/Jobsv1Application.java
@@ -0,0 +1,13 @@
+package com.jobs.jobsv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jobsv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jobsv1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobs/jobsv1/Jobsv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobs.jobsv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jobsv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
